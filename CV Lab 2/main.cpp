514940a7eee3fcdcf13b8a989424bac598090bb1
Conor Taylor <taylorconor@me.com>
2015-10-17 14:57:16 +0100

    Initial Commit

---
+++ CV Lab 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CV Lab 2
+//
+//  Created by Conor Taylor on 17/10/2015.
+//  Copyright © 2015 Conor Taylor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
